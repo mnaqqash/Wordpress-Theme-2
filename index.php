b0c878e06eaa7c7c9083565466e35377cec48c36
Muhammad Naqqash <32413256+naqqasheme@users.noreply.github.com>
2018-10-09 11:40:24 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php get_header(); ?>
+<div class="container">
+	<div class="main">
+		<?php if (have_posts()) :?>
+		<?php while (have_posts()) : the_post(); ?>
+		<article class="post">
+			<h3>
+			<a href="<?php the_permalink(); ?>">
+				<?php the_title(); ?>
+			</a>
+			
+			</h3> 
+			<div class="meta">
+				created By <?php the_author(); ?> on <?php the_date(); ?>
+			</div>
+			<?php if (has_post_thumbnail()): ?>
+			<div class="post-thumbnail">
+				<?php the_post_thumbnail(); ?>
+				
+			</div>
+			<?php endif; ?>
+			<?php the_excerpt(); ?>
+			<br>
+			<a class="button" href="<?php the_permalink(); ?>">
+				Read More
+			</a>
+		</article>
+		<?php endwhile; ?>
+		<?php else : ?>
+		<?php echo wpautop('Sorry, No post found'); ?>
+		<?php endif; ?>
+		
+	</div>
+<div class="sidebar">
+	<?php if(is_active_sidebar( 'sidebar' )): ?>
+		<?php dynamic_sidebar('sidebar'); ?>
+	<?php endif; ?>
+	
+</div>
+<div class="clr">
+	
+</div>
+</div>
+<?php get_footer(); ?>
